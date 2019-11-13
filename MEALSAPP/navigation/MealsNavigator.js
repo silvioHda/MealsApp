9909b315a6f09eb7c1149abe805e3535b7fe9ed6
silvio <silvioignacio_01@hotmail.com>
2019-11-12 22:19:27 -0600

    Meals App del curso de "React Native-the praticalGuide" en udemy

---
+++ MEALSAPP/navigation/MealsNavigator.js
@@ -0,0 +1,16 @@
+ import {
+ 	createStackNavigator,
+ 	createAppContainer
+ 	} from 'react-navigation-stack';
+
+ import CategoriesScreen from '../screens/CategoriesScreen';
+ import CategoryMealsScreen  from '../screens/CategoryMealsScreen';
+ import MealDetailScreen  from '../screens/MealDetailScreen';
+
+ const MealsNavigator = createStackNavigator({
+ 	Categories: CategoriesScreen,
+ 	CategoryMeals: CategoryMealsScreen,
+ 	MealDetail: MealDetailScreen 
+ });
+
+ export default  createAppContainer(MealsNavigator);
---
+++ MEALSAPP/screens/CategoriesScreen.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Button,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  Platform
+} from 'react-native';
+
+import {CATEGORIES} from '../data/dummy-data';
+import Colors from '../constants/Colors';
+
+
+const CategoriesScreen = props =>{
+	const renderGridItem=(itemData)=>{
+	return (
+		<TouchableOpacity 
+		style={styles.gridItem} 
+		onPress={()=>{
+			props.naviga tion.navigate(
+			{
+				routeName:'CategoryMeals', 
+				params: {
+				categoryId: itemData.item.id;
+			} });
+		}}>
+	<View>
+				<Text>{itemData.item.title}</Text>
+			</View>
+			</TouchableOpacity>
+			);
+}
+
+	return(
+
+		<FlatList 
+		keyExtractor={(item,index) => item.id}
+		data={CATEGORIES}
+		renderItem={renderGridItem}
+		numColumns={2} />
+		);
+};
+
+CategoriesScreen.navigationOptions = {
+	headerTitle: 'Meal Categories',
+	headerStyle:{
+		backgroundColor: Platform.OS === 'android' ? Colors.primaryColor: ''
+	},
+	headerTintColor: Platform.OS === 'android' ? 'white': Colors.primaryColor
+};
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	gridItem: {
+		flex: 1,
+		margin: 15,
+		height: 150
+	}
+});
+
+export default CategoriesScreen;
